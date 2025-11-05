b09a2d9e7a6b72d27d31be731b49ccf0f2919e8c
Zeynep Irem Adiguzel <adiguzelzeynepirem@gmail.com>
2025-11-05 11:34:11 +0300

    EX2 başlangıç

---
+++ ex2.py
@@ -0,0 +1,140 @@
+'''def max(list):
+    maxNumber=list[0]
+    for i in list:
+        if maxNumber<i:
+            maxNumber=i
+
+    return maxNumber
+
+print(max([3,5,2,8]))
+
+def opp():
+    str=input("Metin gir: ")
+    str=str[::-1]
+    print(str)
+opp()
+
+def fact(n):
+    result=1
+    while n!=1:
+       result=result*n
+       n-=1
+    print(result)
+fact(6)
+
+def filterEven(list):
+    even=[]
+    for i in list:
+        if i%2==0:
+            even.append(i)
+    print(even)
+filterEven([3,5,7,8])
+
+def mix():
+    list1=[1,2]
+    list=[1,2,1,3]
+    mixed=[]
+    for i in range(0,len(list)):
+        for j in range(0,len(list1)):
+            if list[i]==list1[j]:
+                mixed.append(list[i])
+    print(set(mixed))
+
+mix()
+
+def palindrom(word):
+    if(word==word[::-1]):
+        print("Bu metin palindromdur.")
+    else:
+        print("Metin palindrom değildir")
+palindrom("ala")
+
+def ebob(a,b):
+    lista=[]
+    listb=[]
+    listmax=[]
+    if a>b:
+        for i in range (2,a+1):
+            if(a%i==0):
+                lista.append(i)
+            if(b%i==0):
+                listb.append(i)
+        print(lista,listb)
+        if len(lista)>len(listb):
+            for i in range(0,int(len(lista))):
+                for j in range(0,int(len(listb))):
+                    if(lista[i]==listb[j]):
+                        listmax.append(lista[i])
+            if listmax==[]:
+                print("Ortak bölen yoktur")
+            else:
+                listmax.sort
+                print(listmax)
+                print(listmax[-1])
+        if len(listb)>=len(lista):
+            for i in range(0,int(len(listb))):
+                for j in range(0,int(len(lista))):
+                    if(lista[j]==listb[i]):
+                        listmax.append(lista[j])
+            if listmax==[]:
+                print("Ortak bölen yoktur")
+            else:
+                listmax.sort
+                print(listmax)
+                print(listmax[-1])
+
+            
+    if b>a:
+        for i in range (2,b+1):
+            if(a%i==0):
+                lista.append(i)
+            if(b%i==0):
+                listb.append(i)
+        print(lista,listb)
+        if len(lista)>=len(listb):
+            for i in range(0,int(len(lista))):
+                for j in range(0,int(len(listb))):
+                    if(lista[i]==listb[j]):
+                        listmax.append(listb[j])
+            if listmax==[]:
+                print("Ortak bölen yoktur")
+            else:
+                listmax.sort
+                print(listmax)
+                print(listmax[-1])
+            
+        if len(listb)>len(lista):
+            for i in range(0,int(len(listb))):
+                for j in range(0,int(len(lista))):
+                    if(lista[j]==listb[i]):
+                        listmax.append(lista[j])
+            if listmax==[]:
+                print("Ortak bölen yoktur")
+            else:
+                listmax.sort
+                print(listmax)
+                print(listmax[-1])
+
+    else:
+        if listmax==[]:
+                print("Ortak bölen yoktur")
+        
+
+    
+ebob(13,32)  '''
+
+def anagram(str,str1):
+    if len(str)==len(str1):
+        list=[]
+        for i in str:
+            for j in str1:
+                if i==j:
+                    list.append(i)
+        print(list)
+        if len(list)==len(str):
+            
+            return True
+        else: return False
+    else:
+        return False
+print(anagram("hello","olleh"))
