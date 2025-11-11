56296ae8492284d14405e597aa099e7e771b2f47
Zeynep Irem Adiguzel <adiguzelzeynepirem@gmail.com>
2025-11-11 16:29:48 +0300

    .

--- ex2.py
+++ ex2.py
@@ -121,11 +121,14 @@ def ebob(a,b):
         
 
     
-ebob(13,32)  '''
+ebob(13,32)  
 
 def anagram(str,str1):
     if len(str)==len(str1):
         list=[]
+        str=set(str)
+        str1=set(str1)
+        print(str,str1)
         for i in str:
             for j in str1:
                 if i==j:
@@ -138,3 +141,91 @@ def anagram(str,str1):
     else:
         return False
 print(anagram("hello","olleh"))
+
+def isExist(list):
+    for i in range(0,list[-1]):
+        if i+1==list[i]:
+            continue
+        else:
+            print(f"{i+1} eksik eleman")
+            break
+        
+isExist([1,2,3,5])
+
+def rotate(k):
+    list=[1,2,3,7,6,8]
+    end=[]
+    first=[]
+    for i in range(k+1,len(list)):
+        end.append(list[i])
+    for i in range(0,k+1):
+        first.append(list[i])
+    print(end+first)
+
+rotate(2)
+def median(list,list1):
+    result=(list+list1)
+    result.sort
+    print(result)
+    if len(result)%2==0:
+        med=float(result[int(len(result)/2)]+result[int(len(result)/2)-1])/2
+    else:
+        med=float(result[int(len(result)/2)])
+    print(med)
+    
+median([1,2,3],[4,22,23])
+
+def longestPalindrom(str):
+    result=[]
+    str1=str[::-1]
+    print(str1)
+    if str==str[::-1]:
+        print(str)
+    for i in str:
+        for j in str1:
+            if i==j:
+                #print(i)
+                if j==str1[len(str)-1]:
+                    print(str[len(str)-1])
+                    break
+                else:
+                    result.append(j)
+                    
+                    break
+    print(result)
+                
+
+    
+longestPalindrom("kalama")'''
+
+def longestPalindrome(s):
+    if not s:
+        return ""
+    
+    start = 0
+    max_len = 1
+    
+    def expand_around_center(left, right):
+        # Merkez etrafında palindromu genişlet
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1  # Palindromun uzunluğu
+    
+    for i in range(len(s)):
+        # Tek uzunluklu palindrom (merkez bir karakter)
+        len1 = expand_around_center(i, i)
+        # Çift uzunluklu palindrom (merkez iki karakter arası)
+        len2 = expand_around_center(i, i + 1)
+        
+        # En uzun palindromu güncelle
+        current_max = max(len1, len2)
+        if current_max > max_len:
+            max_len = current_max
+            start = i - (current_max - 1) // 2
+    
+    return s[start:start + max_len]
+
+# Test
+print(longestPalindrome("babab"))
+# Çıktı: "bab" veya "aba"
---
+++ homeWorks/numpyHomework.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+list=np.arrange(9)
+print(list)
+
+list1=np.array([np.zeros(4)],[np.zeros(4)],[np.zeros(4)])
+
+list2=np.array([np.ones(4)],[np.ones(4)],[np.ones(4)])
